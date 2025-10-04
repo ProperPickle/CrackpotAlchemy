339fff347a228e911326e0b6c00a1a1fbeea9db2
BitsNinja <iandavis07@icloud.com>
2025-10-03 19:51:29 -0700

    Cleanup and debugging things

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -26,7 +26,6 @@ export class Game extends Scene
     }
 
     //private field 
-    //#platforms:Phaser.Physics.Arcade.StaticGroup;
     player:Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
     create (){
         // loaded via public/assets
@@ -34,44 +33,17 @@ export class Game extends Scene
         // stretch asset
         bg.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height)
 
-        //let star = this.physics.add.staticImage(400, 300, 'star')
-
-        //different way of defining private variables. no this.player, however variable is scoped to create(){}
+        // Defining the item
         this.player = this.physics.add.sprite(450, 450, 'dude');
         this.player.scale *= 2
-        this.player.setMaxVelocity(3000)
 
-        /*
-        this.physics.add.overlap(this.player, star, function(){
-            star.destroy(true)
-        });
-        */
-        //player physics
         this.player.setBounce(0.2);
         this.player.setCollideWorldBounds(true);
-        this.player.setGravityY(0);
-        //this.physics.add.collider(this.player, this.#platforms);
-
-
-        //animations for player
-        this.anims.create({
-            key: 'left',
-            frames: this.anims.generateFrameNumbers('dude', { start: 0, end: 3 }),
-            frameRate: 10,
-            repeat: -1
-        });
 
         this.anims.create({
             key: 'turn',
             frames: [ { key: 'dude', frame: 4 } ],
             frameRate: 20
-        });
-
-        this.anims.create({
-            key: 'right',
-            frames: this.anims.generateFrameNumbers('dude', { start: 5, end: 8 }),
-            frameRate: 10,
-            repeat: -1
         });
 
         this.player.anims.play('turn');
@@ -81,33 +53,60 @@ export class Game extends Scene
     }
 
     update() {
+        // This is purely in here for debugging
+        let keyboard = this.input.keyboard
+        let cursors;
+        if(keyboard!= null)
+            cursors = keyboard.createCursorKeys();
+        else
+            throw new Error("no keyboard")
+        
+        if (cursors.left.isDown){
+            this.player.body.x -= 10
+        }
+        else if (cursors.right.isDown){
+            this.player.body.x += 10
+        }
+        else if (cursors.up.isDown){
+            this.player.body.y -= 10
+        }
+        else if (cursors.down.isDown){
+            this.player.body.y += 10
+        }
+        // end purely in here for debug
+
+
+
         const mouse = this.input.mousePointer;
         if (!mouse) return;
 
         const center = new Phaser.Math.Vector2(this.sys.canvas.width/2, this.sys.canvas.height/2);
         const maxRadius = 200;
 
-        let target = new Phaser.Math.Vector2(mouse.x, mouse.y);
+        let mousePos = new Phaser.Math.Vector2(mouse.x, mouse.y);
 
-        let offset = target.clone().subtract(center);
+        let mouseToPlayer = mousePos.clone().subtract(center);
 
-        if (offset.length() > maxRadius) {
-            offset = offset.normalize().scale(maxRadius);
+        if (mouseToPlayer.length() > maxRadius) {
+            mouseToPlayer = mouseToPlayer.normalize().scale(maxRadius);
         }
 
-        const clampedPos = center.clone().add(offset);
+        const clampedMousePos = center.clone().add(mouseToPlayer);
 
         if (mouse.primaryDown) {
+
+            // Loop all items to check for clicks
             for (let item of this.items) {
-                if (item.getBounds().contains(mouse.x, mouse.y)) {
-                    if (!this.isDragging.includes(this.player)) {
+                if (item.getBounds().contains(clampedMousePos.x, clampedMousePos.y) && !this.isDragging.includes(this.player)) {
+                    
                         this.isDragging.push(this.player)
+
                         if (this.isDropped.includes(this.player)) {
                             this.isDropped.splice(this.isDropped.indexOf(this.player), 1)
                         }
-                    }
                 }
             }
+
         } else {
             this.isDragging.forEach(element => {
                 this.isDropped.push(element)
@@ -115,43 +114,37 @@ export class Game extends Scene
             this.isDragging = []
         }
         
-
+        // Move currently dragged objects (option to have multiple at once with this?)
         for (let object of this.isDragging) {
 
-            let toTarget = clampedPos.clone().subtract(this.player.getCenter())
+            let toTarget = clampedMousePos.clone().subtract(this.player.getCenter())
 
             const maxSpeed = 3000
             const distance = toTarget.length()
             const speed = Math.min(distance * 10, maxSpeed)
             let desiredVelocity = toTarget.clone().normalize().scale(speed)
 
+            // Smooth velocity changes
             const k = 0.3; 
             object.body.velocity.x += (desiredVelocity.x - object.body.velocity.x) * k
             object.body.velocity.y += (desiredVelocity.y - object.body.velocity.y) * k
-
         }
 
         for (let i = this.isDropped.length - 1; i >= 0; i--) {
             const object = this.isDropped[i]
 
+            // Drag
             const k = 0.1;
             object.body.velocity.x -= object.body.velocity.x * k
             object.body.velocity.y -= object.body.velocity.y * k
 
+            // Once item is slowed enough, stop and remove it as a sliding item
             if (object.body.speed < 10) {
                 this.isDropped.splice(i, 1)
                 object.body.setVelocity(0)
             }
         }
 
-
         
-    }
-
-    clickWithinRadius(sprite: Phaser.Types.Physics.Arcade.SpriteWithDynamicBody, radius: number) {
-        return sprite.scene.input.mousePointer.position.distance(
-            new Phaser.Math.Vector2(sprite.x, sprite.y))
-            <= radius
-
     }
 }
