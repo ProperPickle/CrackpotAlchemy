89308080bd711a3ffc070aaea84e44959e8447b9
Colin Doherty <colinjdoherty@outlook.com>
2025-10-04 12:17:22 -0700

    abstract v1

--- src/game/scenes/GameImpls/Controllables.ts
+++ src/game/scenes/GameImpls/Controllables.ts
@@ -1,4 +1,6 @@
 import {Game} from '../Game'
+
+
 function addControllables(){
     Game.prototype.createPlayer = function(){
         this.player = this.physics.add.sprite(100, 450, 'dude');
@@ -32,6 +34,147 @@ function addControllables(){
         this.item2.setCollideWorldBounds(true);
 
         this.items.push(this.item2)
+    }
+
+    Game.prototype.repelObjects = function(objects, repulsionRadius, strength) {
+        for (let i = 0; i < objects.length; i++) {
+            for (let j = i + 1; j < objects.length; j++) {
+                const a = objects[i];
+                const b = objects[j];
+
+                const delta = a.body.position.clone().subtract(b.body.position);
+                const distance = delta.length();
+
+                if (distance === 0) {
+                    const force = Phaser.Math.RandomXY(new Phaser.Math.Vector2)
+
+                    // apply equal and opposite velocity changes
+                    a.body.velocity.add(force);
+                    b.body.velocity.subtract(force);
+
+                    if (!this.isDragging.includes(a)) this.isThrown.push(a)
+                    if (!this.isDragging.includes(b)) this.isThrown.push(b)
+                };
+
+                if (distance < repulsionRadius) {
+                    const force = delta.normalize().scale((repulsionRadius - distance) * strength);
+
+                    // apply equal and opposite velocity changes
+                    a.body.velocity.add(force);
+                    b.body.velocity.subtract(force);
+
+                    if (!this.isDragging.includes(a)) this.isThrown.push(a)
+                    if (!this.isDragging.includes(b)) this.isThrown.push(b)
+                }
+        
+            }
+        }
+    }
+
+    Game.prototype.controlItems = function(){
+        const mouse = this.input.mousePointer;
+        if (!mouse) return;
+
+        const center = this.player.body.position
+        const maxRadius = 200;
+
+        let mousePos = new Phaser.Math.Vector2(mouse.worldX, mouse.worldY);
+
+        let mouseToPlayer = mousePos.clone().subtract(center);
+
+        if (mouseToPlayer.length() > maxRadius) {
+            mouseToPlayer = mouseToPlayer.normalize().scale(maxRadius);
+        }
+
+        const clampedMousePos = center.clone().add(mouseToPlayer);
+
+        this.repelObjects(this.items, 50, 20);
+
+        if (mouse.primaryDown) {
+
+            // Loop all items to check for clicks
+            for (let item of this.items) {
+                if (item.getBounds().contains(clampedMousePos.x, clampedMousePos.y) && !this.isDragging.includes(item)) {
+                    
+                        this.isDragging.push(item)
+
+                        if (this.isThrown.includes(item)) {
+                            this.isThrown.splice(this.isThrown.indexOf(item), 1)
+                        }
+                }
+            }
+
+        } else {
+            this.isDragging.forEach(element => {
+                this.isThrown.push(element)
+            });
+            this.isDragging = []
+        }
+        for (let object of this.isDragging) {
+
+            let toTarget = clampedMousePos.clone().subtract(object.getCenter())
+
+            const maxSpeed = 4000
+            const distance = toTarget.length()
+            const speed = Math.min(distance * 10, maxSpeed)
+            let desiredVelocity = toTarget.clone().normalize().scale(speed)
+
+            // Smooth velocity changes
+            const k = 0.3; 
+            object.body.velocity.x += (desiredVelocity.x - object.body.velocity.x) * k
+            object.body.velocity.y += (desiredVelocity.y - object.body.velocity.y) * k
+        }
+
+        for (let i = this.isThrown.length - 1; i >= 0; i--) {
+            const object = this.isThrown[i]
+
+            // Drag
+            const k = 0.1;
+            object.body.velocity.x -= object.body.velocity.x * k
+            object.body.velocity.y -= object.body.velocity.y * k
+
+            // Once item is slowed enough, stop and remove it as a thrown item
+            if (object.body.velocity.length() < 10) {
+                this.isThrown.splice(i, 1)
+                object.body.setVelocity(0)
+            }
+        }
+    }
+
+    Game.prototype.movePlayer = function(){
+        let keyboard = this.input.keyboard
+        let cursors;
+        if(keyboard!= null)
+            cursors = keyboard.createCursorKeys();
+        else
+            throw new Error("no keyboard")
+        
+        if (cursors.left.isDown&&!cursors.right.isDown){
+            this.player.setVelocityX(-160);
+        } else if (cursors.right.isDown&&!cursors.left.isDown){
+            this.player.setVelocityX(160);
+        } else if (cursors.up.isDown&&!cursors.down.isDown){
+            this.player.setVelocityY(-160);
+        } else if (cursors.down.isDown&&!cursors.up.isDown){
+            this.player.setVelocityY(160);
+        } else {
+            this.player.setVelocity(0);
+        }
+    }
+
+    Game.prototype.logTile = function(){
+        //changed to pointerdown so it shouldn't blow up the console
+        this.input.on('pointerdown', (pointer: Phaser.Input.Pointer) => {
+            // Get the tile at the pointer's world position
+            const tile = this.platforms.getTileAtWorldXY(pointer.worldX, pointer.worldY);
+            if (tile) {
+                // Display the tile's index (for example, in the console or on-screen)
+                console.log("Tile index:", tile.index);
+                // Optionally display it on screen using a text object
+                // this.indexText.setText('Tile index: ' + tile.index);
+                // this.indexText.setPosition(pointer.x + 10, pointer.y + 10);
+            }
+        });
     }
 }
 
