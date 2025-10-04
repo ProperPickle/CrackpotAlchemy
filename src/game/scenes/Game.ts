bed23cf146b6c7a000c4d90c9c17e76d5c612ee3
ProperPickle <darthhugobacon@gmail.com>
2025-10-03 20:02:47 -0700

    Tilemap + Camera Following + Changing Orientation

--- src/game/scenes/Game.ts
+++ src/game/scenes/Game.ts
@@ -18,38 +18,49 @@ export class Game extends Scene
             'assets/dude.png',
             { frameWidth: 32, frameHeight: 48 }
         );
+        this.load.image('tiles', 'assets/tilemaps/tileset.png');
+        this.load.tilemapTiledJSON('map', 'assets/tilemaps/world.json');
     }
 
-    //private field 
-    #platforms:Phaser.Physics.Arcade.StaticGroup;
+    //private field
+    #platforms: Phaser.Tilemaps.TilemapLayer | null = null;
     player:Phaser.Types.Physics.Arcade.SpriteWithDynamicBody
     create (){
+        
         // loaded via public/assets
         let bg = this.add.image(0, 0, 'sky').setOrigin(0, 0)
+        //Adding in tilemap
+        const myMap = this.make.tilemap({ key: 'map' });
+        // the first arg must match the "name" of the tileset in Tiled (see public/assets/tilemaps/world.json)
+        const tileset = myMap.addTilesetImage('fucked_up_chungus_world', 'tiles');
         // stretch asset
         bg.setDisplaySize(this.sys.canvas.width, this.sys.canvas.height)
+        let camera = this.cameras.main;
 
-        let star = this.physics.add.staticImage(400, 300, 'star')
-
-        this.#platforms = this.physics.add.staticGroup();
-
-        this.#platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-        this.#platforms.create(600, 400, 'ground');
-        this.#platforms.create(50, 250, 'ground');
-        this.#platforms.create(750, 220, 'ground');
-
+        let star = this.physics.add.staticImage(400, 300, 'star');
+        if (!tileset) {
+            throw new Error("Tileset 'map' with image key 'tiles' not found.");
+        }
+    const platforms = myMap.createLayer('Floor', tileset, 0, 0);
+        if (!platforms) {
+            throw new Error("Tilemap layer 'Platforms' not found or failed to create.");
+        }
+        // store layer and enable collisions for non-empty tiles
+        this.#platforms = platforms;
+        this.#platforms.setCollisionByExclusion([1], true);
+        
         //different way of defining private variables. no this.player, however variable is scoped to create(){}
         this.player = this.physics.add.sprite(100, 450, 'dude');
 
         this.physics.add.overlap(this.player, star, function(){
             star.destroy(true)
         });
+        camera.startFollow(this.player, true, 0.5, 0.5);
         //player physics
         this.player.setBounce(0.2);
-        this.player.setCollideWorldBounds(true);
-        this.player.setGravityY(400);
-        this.physics.add.collider(this.player, this.#platforms);
+        this.player.setCollideWorldBounds(false);
+        // platforms is definitely non-null here because of the guard above
+        this.physics.add.collider(this.player, this.#platforms as Phaser.Tilemaps.TilemapLayer);
 
         //animations for player
         this.anims.create({
@@ -71,7 +82,20 @@ export class Game extends Scene
             frameRate: 10,
             repeat: -1
         });
-        
+        // Function for finding tile indices (blows the console the fuck up).
+        /*
+        this.input.on('pointermove', (pointer: Phaser.Input.Pointer) => {
+        // Get the tile at the pointer's world position
+        const tile = this.#platforms.getTileAtWorldXY(pointer.worldX, pointer.worldY);
+        if (tile) {
+            // Display the tile's index (for example, in the console or on-screen)
+            console.log("Tile index:", tile.index);
+            // Optionally display it on screen using a text object
+            // this.indexText.setText('Tile index: ' + tile.index);
+            // this.indexText.setPosition(pointer.x + 10, pointer.y + 10);
+        }
+        });
+        */
     }
 
     update(){
@@ -82,12 +106,12 @@ export class Game extends Scene
         else
             throw new Error("no keyboard")
         
-        if (cursors.left.isDown){
+        if (cursors.left.isDown&&!cursors.right.isDown){
             this.player.setVelocityX(-160);
 
             this.player.anims.play('left', true);
         }
-        else if (cursors.right.isDown){
+        else if (cursors.right.isDown&&!cursors.left.isDown){
             this.player.setVelocityX(160);
 
             this.player.anims.play('right', true);
@@ -98,8 +122,12 @@ export class Game extends Scene
             this.player.anims.play('turn');
         }
 
-        if (cursors.up.isDown && this.player.body.touching.down){
-            this.player.setVelocityY(-550);
+        if (cursors.up.isDown&&!cursors.down.isDown){
+            this.player.setVelocityY(-160);
+        } else if (cursors.down.isDown&&!cursors.up.isDown){
+            this.player.setVelocityY(160);
+        } else{
+            this.player.setVelocityY(0);
         }
     }
 }
